4d86773ba3410a8af2cfbb4e1288c1bfca7b157e
LeeSeungnam <dltmdska01@naver.com>
2023-11-26 20:34:53 +0900

    섹션 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -13,25 +13,32 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
+import Main from './components/section/Main'
+import Header from './components/section/Header'
+import Footer from './components/section/Footer'
 
 const App = () => {
    return (
       <BrowserRouter>
-         <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/today' element={<Today/>}></Route>
-            <Route path='/developer' element={<Developer/>}></Route>
-            <Route path='/developer' element={<Developer/>}></Route>
-            <Route path='/webd' element={<Webd/>}></Route>
-            <Route path='/website' element={<Website/>}></Route>
-            <Route path='/gsap' element={<Gsap/>}></Route>
-            <Route path='/port' element={<Port/>}></Route>
-            <Route path='/youtube' element={<Youtube/>}></Route>
-            <Route path='/channel/:channelID' element={<Channel/>}></Route>
-            <Route path='/Video/:VideoID' element={<Video/>}></Route>
-            <Route path='/search' element={<Search/>}></Route>
-            <Route path='/*' element={<Not/>}></Route>
-         </Routes>
+         <Header/>
+         <Main>
+            <Routes>
+               <Route path='/' element={<Home/>}></Route>
+               <Route path='/today' element={<Today/>}></Route>
+               <Route path='/developer' element={<Developer/>}></Route>
+               <Route path='/developer' element={<Developer/>}></Route>
+               <Route path='/webd' element={<Webd/>}></Route>
+               <Route path='/website' element={<Website/>}></Route>
+               <Route path='/gsap' element={<Gsap/>}></Route>
+               <Route path='/port' element={<Port/>}></Route>
+               <Route path='/youtube' element={<Youtube/>}></Route>
+               <Route path='/channel/:channelID' element={<Channel/>}></Route>
+               <Route path='/Video/:VideoID' element={<Video/>}></Route>
+               <Route path='/search' element={<Search/>}></Route>
+               <Route path='/*' element={<Not/>}></Route>
+            </Routes>
+         </Main>
+         <Footer/>
       </BrowserRouter>
    )
 }
