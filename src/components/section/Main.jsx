be1e288f7e3f807e86c60d8e24d551362148bf1d
LeeSeungnam <dltmdska01@naver.com>
2023-11-30 19:45:36 +0900

    페이지 SEO 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Helmet,HelmetProvider} from'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
@@ -6,13 +7,21 @@ import Footer from './Footer'
 const Main = (props) => {
 
     return (
-        <>
+        <HelmetProvider>
+            <Helmet
+                titleTemplate="%s|이승남 Youtube"
+                defaultTitle="이승남 Youtube"
+                defer={false}
+            >
+                {props.title && <title>{props.title}</title>}
+                <meta name="description" content={props.description}/>
+            </Helmet>
             <Header/>
             <main id='main' role='main'>
                     {props.children}
             </main>
             <Footer/>
-        </>
+        </HelmetProvider>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../components/section/Main'
 
 const Home = () => {
     return (
-        <Main>
+        <Main title="이승남 유튜브" description="유튜브 채널 페이지입니다.">
             Home
         </Main>
     )
